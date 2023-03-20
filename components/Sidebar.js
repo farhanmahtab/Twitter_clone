1ba928504a88a4ade923a97548cbb6fedf78228e
mahi.shellbeehaken@gmail.com <mahi.shellbeehaken@gmail.com>
2023-03-20 13:00:21 +0600

    next init + icon&logo added

---
+++ components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from "next/image";
+
+function Sidebar() {
+  return (
+    <div>
+        sidebar
+      {/* logo */}
+      <div className="logo"><Image width="50" height="50" src="/Resource/Twitter-logo.ico"></Image>
+      </div>
+      {/* menu */}
+      {/* button */}
+      {/* profile */}
+    </div>
+  );
+}
+
+export default Sidebar;
